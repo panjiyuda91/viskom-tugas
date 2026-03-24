65e998dae3016944fb82fe54947122ae508e0f6a
panjiyuda91 <146803196+panjiyuda91@users.noreply.github.com>
2026-03-25 05:19:38 +0800

    Update deteksiTepi.py

--- tugas_3/deteksiTepi.py
+++ tugas_3/deteksiTepi.py
@@ -4,27 +4,25 @@ import math
 def konvolusi_tepi(path_gambar, output_gambar, metode="sobel"):
     img = Image.open(path_gambar).convert("RGB")
     lebar, tinggi = img.size
-    img_baru = Image.new("L", (lebar, tinggi)) # 'L' untuk mode Grayscale (Hitam Putih)
+    img_baru = Image.new("L", (lebar, tinggi))
     
     pixels_lama = img.load()
     pixels_baru = img_baru.load()
 
-    # 1. Konversi ke Grayscale (Rumus Luminance)
     gray_matrix = [[0] * lebar for _ in range(tinggi)]
     for y in range(tinggi):
         for x in range(lebar):
             r, g, b = pixels_lama[x, y]
             gray_matrix[y][x] = int(0.299*r + 0.587*g + 0.114*b)
 
-    # 2. Definisi Kernel Super Elemen 3x3
     if metode == "sobel":
         Kx = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
         Ky = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
-    else: # prewitt
+    else: # Prewitt
         Kx = [[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]]
         Ky = [[-1, -1, -1], [0, 0, 0], [1, 1, 1]]
 
-    print(f"Memproses Deteksi Tepi ({metode.capitalize()})... Ini mungkin butuh waktu beberapa detik.")
+    print(f"Deteksi Tepi ({metode.capitalize()})")
     
     # 3. Operasi Konvolusi
     # Kita mulai dari indeks 1 hingga panjang-1 untuk menghindari error di ujung/tepi matriks (border)
@@ -46,7 +44,7 @@ def konvolusi_tepi(path_gambar, output_gambar, metode="sobel"):
             pixels_baru[x, y] = max(0, min(255, magnitude))
             
     img_baru.save(output_gambar)
-    print(f"Selesai! Disimpan sebagai {output_gambar}")
+    print(f"{output_gambar}")
 
 if __name__ == "__main__":
     konvolusi_tepi("gambar.jpg", "tepiSobel.jpg", metode="sobel")
